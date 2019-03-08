328cadb1a0a22d34232d40be30723c532a6ccf5f
uria <ryouma_1106@yahoo.co.jp>
2019-03-08 18:36:58 +0900

    上下左右斜めに移動できるプログラムを追加 おやつはカントリーマアム

---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] Sprite[] PlayerSprite;
+    [SerializeField] SpriteRenderer MainSpriteRenderer;
+
+    [SerializeField] float MovePower;//速度
+
+    [SerializeField] int PlayerDirection;//向いている方向　0:上 1:右 2:左 3:下
+
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();//このオブジェクトのSpriteRendererを取得
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+    }
+
+
+    void Move()
+    {
+        Vector3 MoveLate = new Vector3 (0, 0, 0);
+        float MoveLateX = 0, MoveLateY = 0;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            // transform.Translate(MovePower, 0, 0);
+            MoveLateX = MovePower;
+            MainSpriteRenderer.sprite = PlayerSprite[1];
+            PlayerDirection = 1;
+        }
+        /*else*/ if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //transform.Translate(-MovePower, 0, 0);
+            MoveLateX = -MovePower;
+            MainSpriteRenderer.sprite = PlayerSprite[2];
+            PlayerDirection = 2;
+        }
+        /*else*/ if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //transform.Translate(0, MovePower, 0);
+            MoveLateY = MovePower;
+            MainSpriteRenderer.sprite = PlayerSprite[0];
+            PlayerDirection = 0;
+
+        }/*else*/ if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //transform.Translate(0, -MovePower, 0);
+            MoveLateY = -MovePower;
+            MainSpriteRenderer.sprite = PlayerSprite[3];
+            PlayerDirection = 3;
+        }
+
+        transform.Translate(MoveLateX, MoveLateY, 0);
+    }
+}
