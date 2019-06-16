0c0c056e33a82284571492491d6396e8bd0446de
uria <ryouma_1106@yahoo.co.jp>
2019-06-16 16:45:47 -0700

    壊すとアイテムの出る木箱の実装、鎧君のあたり判定の実装（rigidbody2dの付け忘れだった) これからも頑張っていきたいコミットコミット

---
+++ Assets/Script/WoodBox.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodBox : MonoBehaviour
+{
+    public int Hp = 1;
+    public bool ItemCheck;//何かアイテムが入っているかチェックするよう
+    public int ItemNumber;//アイテムの種類（入っている場合のみ使うかも？）
+    public GameObject[] Items;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Hp <= 0)
+        {
+            if (ItemCheck)//何かアイテムがあるならここに出す
+            {
+                Instantiate(Items[ItemNumber], this.transform.position, Quaternion.identity);
+            }
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Shot")//自キャラのショットに当たったら～
+        {
+            Hp--;
+        }
+    }
+}
